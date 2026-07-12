7f25154a5d44c209c4d7dea859be371372c7e0f6
Noam de Villeneuve Bargemon <noamvb1@gmail.com>
2026-07-12 14:52:01 -0400

    Prepare Cannsheet Mobile 1.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
     applicationId = "com.noamv.cannsheet.mobile"
     minSdk = 24
     targetSdk = 36
-    versionCode = 4
-    versionName = "1.2.1"
+    versionCode = 5
+    versionName = "1.2.2"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
