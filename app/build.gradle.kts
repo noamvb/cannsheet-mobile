1becb35ef5511bd5fb99bbdaaa1717494edfed8d
Noam de Villeneuve Bargemon <noamvb1@gmail.com>
2026-08-11 18:31:36 -0400

    Release v1.2.23

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
     applicationId = "com.noamv.cannsheet.mobile"
     minSdk = 24
     targetSdk = 36
-    versionCode = 25
-    versionName = "1.2.22"
+    versionCode = 26
+    versionName = "1.2.23"
 
     buildConfigField("String", "GAS_URL", buildConfigString(productionGasUrl))
     buildConfigField("String", "APP_ENVIRONMENT", buildConfigString("PRODUCTION"))
